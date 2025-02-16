e2d028a25b623742a1bca15c4ffb092c1dd6b9ec
mosahle7 <103498044+mosahle7@users.noreply.github.com>
2025-02-16 05:34:33 +0000

    Change in App

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return(
     <>
   
-    <BrowserRouter>
+    <BrowserRouter basename="/mst-image-compressor">
     <Routes>
       <Route path = '/' element = {<Layout/>}>
       <Route index element={<ImageUploader/>}/>
