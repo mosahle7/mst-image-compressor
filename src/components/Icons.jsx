1da043b192dc4be45323c2fb4ab17adec21136df
mosahle7 <103498044+mosahle7@users.noreply.github.com>
2025-02-24 16:34:22 +0000

    Sun or Dark!

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faSunBright } from '@fortawesome/pro-solid-svg-icons';
 
 export const HomeIcon = ({width, onClick}) => <svg width={width} onClick={onClick} stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 576 512" height="1em" xmlns="http://www.w3.org/2000/svg"><path d="M280.37 148.26L96 300.11V464a16 16 0 0 0 16 16l112.06-.29a16 16 0 0 0 15.92-16V368a16 16 0 0 1 16-16h64a16 16 0 0 1 16 16v95.64a16 16 0 0 0 16 16.05L464 480a16 16 0 0 0 16-16V300L295.67 148.26a12.19 12.19 0 0 0-15.3 0zM571.6 251.47L488 182.56V44.05a12 12 0 0 0-12-12h-56a12 12 0 0 0-12 12v72.61L318.47 43a48 48 0 0 0-61 0L4.34 251.47a12 12 0 0 0-1.6 16.9l25.5 31A12 12 0 0 0 45.15 301l235.22-193.74a12.19 12.19 0 0 1 15.3 0L530.9 301a12 12 0 0 0 16.9-1.6l25.5-31a12 12 0 0 0-1.7-16.93z"></path></svg>
 export const DownIcon = ({width, onClick}) => <svg width={width} onClick={onClick} stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 1024 1024" height="1em" xmlns="http://www.w3.org/2000/svg"><path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm192 472c0 4.4-3.6 8-8 8H328c-4.4 0-8-3.6-8-8v-48c0-4.4 3.6-8 8-8h368c4.4 0 8 3.6 8 8v48z"></path></svg>
@@ -27,4 +29,63 @@ export const DownloadIcon = ({ width, onClick }) => (
       <path d="M64 0C28.7 0 0 28.7 0 64L0 448c0 35.3 28.7 64 64 64l256 0c35.3 0 64-28.7 64-64l0-288-128 0c-17.7 0-32-14.3-32-32L224 0 64 0zM256 0l0 128 128 0L256 0zM216 232l0 102.1 31-31c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-72 72c-9.4 9.4-24.6 9.4-33.9 0l-72-72c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l31 31L168 232c0-13.3 10.7-24 24-24s24 10.7 24 24z" />
     </BounceSVG>
   );
-  
+
+// export const SunIcon = () => {
+//   return (
+//     <FontAwesomeIcon 
+//       icon={faSunBright}
+//       fade
+//       style={{ color: "#ffffff"}}
+//     />
+//   )
+// }
+
+
+export const SunIcon = ({ width, onClick }) => (
+  <svg
+    fill="white"
+    stroke="white"
+    width = {width}
+    onClick = {onClick}
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    strokeWidth="2"
+    // className={`w-${width} h-${height} stroke-${color}`}
+    viewBox="0 0 24 24"
+  >
+    <path d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
+    <path d="M12 4v1m5.66 1.344l-.828.828m3.173 4.832h-1m-1.345 5.66l-.828-.828M12 20.01V19m-5.66-1.336l.835-.836m-3.18-4.824h1.01M6 6l.835.836"></path>
+  </svg>
+);
+
+
+export const DarkIcon = ({ width, onClick}) => (
+  <svg
+    viewBox="0 0 24 24"
+    fill="none"
+    // className={`w-${width} h-${height}`}
+    width = {width}
+    onClick={onClick}
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M17.715 15.15A6.5 6.5 0 0 1 9 6.035C6.106 6.922 4 9.645 4 12.867c0 3.94 3.153 7.136 7.042 7.136 3.101 0 5.734-2.032 6.673-4.853Z"
+      className="fill-transparent"
+    />
+    <path
+      d="m17.715 15.15.95.316a1 1 0 0 0-1.445-1.185l.495.869ZM9 6.035l.846.534a1 1 0 0 0-1.14-1.49L9 6.035Zm8.221 8.246a5.47 5.47 0 0 1-2.72.718v2a7.47 7.47 0 0 0 3.71-.98l-.99-1.738Zm-2.72.718A5.5 5.5 0 0 1 9 9.5H7a7.5 7.5 0 0 0 7.5 7.5v-2ZM9 9.5c0-1.079.31-2.082.845-2.93L8.153 5.5A7.47 7.47 0 0 0 7 9.5h2Zm-4 3.368C5 10.089 6.815 7.75 9.292 6.99L8.706 5.08C5.397 6.094 3 9.201 3 12.867h2Zm6.042 6.136C7.718 19.003 5 16.268 5 12.867H3c0 4.48 3.588 8.136 8.042 8.136v-2Zm5.725-4.17c-.81 2.433-3.074 4.17-5.725 4.17v2c3.552 0 6.553-2.327 7.622-5.537l-1.897-.632Z"
+      // className={`fill-${color}`}
+      fill="white"
+    />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M17 3a1 1 0 0 1 1 1 2 2 0 0 0 2 2 1 1 0 1 1 0 2 2 2 0 0 0-2 2 1 1 0 1 1-2 0 2 2 0 0 0-2-2 1 1 0 1 1 0-2 2 2 0 0 0 2-2 1 1 0 0 1 1-1Z"
+      // className={`fill-${color}`}
+      fill="white"
+    />
+  </svg>
+);
+
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,17 +1,42 @@
 import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
+import { useState } from 'react';
 import styled from 'styled-components';
-import { HomeIcon } from './Icons';
+import { DarkIcon, HomeIcon, SunIcon } from './Icons';
 
 export const Layout = () => {
+
+    const [light, setLight] = useState(true);
+
+    const handleLight = () => {
+        if(light){
+        setLight(false);
+        console.log("I am dark!");
+        document.documentElement.style.setProperty('--background-color', 'var(--background-color-dark)');
+        }
+        else{
+        setLight(true);
+        console.log("I am light!");
+        document.documentElement.style.setProperty('--background-color', 'var(--background-color-light)');
+        }
+    }
+
     return(
     <AppContainer>
     <header>
         <HeaderHomeIcon>
             <Link to='/'><HomeIcon width={40}/></Link>
         </HeaderHomeIcon>
+        
 
         <Title> mSt Image Compressor </Title>
+        {light ?(
+        <SunIcon width={40} onClick={handleLight}/>
+        ) :
+        (!light &&
+        <DarkIcon width={40} onClick={handleLight}/>
+        )
+    }
 
     </header>
     
@@ -19,6 +44,7 @@ export const Layout = () => {
         <nav>
 
             <ul>
+                Test
     
             </ul>
         </nav>
